353ba8eb3f3e923f0eabd18d6fbde715cb4e6434
Ngeorge <142554602+nicolergeorge@users.noreply.github.com>
2024-10-07 11:40:29 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function updateTime() {
         let losAngelesTimeElement = losAngelesElement.querySelector(".time");
         let losAngelesTime = moment().tz("America/Los_Angeles");
 
-        losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
+        losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
         losAngelesTimeElement.innerHTML = losAngelesTime.format(
             "h:mm:ss [<small>]A[</small>]"
         );
@@ -19,33 +19,54 @@ function updateTime() {
         let parisTimeElement = parisElement.querySelector(".time");
         let parisTime = moment().tz("Europe/Paris");
 
-        parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
+        parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
         parisTimeElement.innerHTML = parisTime.format(
+            "h:mm:ss [<small>]A[</small>]"
+        );
+    }
+
+    // Tokyo (new timezone)
+    let tokyoElement = document.querySelector("#tokyo");
+    if (tokyoElement) {
+        let tokyoDateElement = tokyoElement.querySelector(".date");
+        let tokyoTimeElement = tokyoElement.querySelector(".time");
+        let tokyoTime = moment().tz("Asia/Tokyo");
+
+        tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+        tokyoTimeElement.innerHTML = tokyoTime.format(
             "h:mm:ss [<small>]A[</small>]"
         );
     }
 }
 
 function updateCity(event) {
+    clearInterval(selectedCityInterval);  // Clear any previous intervals
+
     let cityTimeZone = event.target.value;
     if (cityTimeZone === "current") {
-        cityTimeZone = moment.tz.guess();
+        cityTimeZone = moment.tz.guess();  // Guess user's timezone
     }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-    let cityTime = moment().tz(cityTimeZone);
-    let citiesElement = document.querySelector("#cities");
-    citiesElement.innerHTML = `
-  <div class="city">
-    <div>
-      <h2>${cityName}</h2>
-      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
-    </div>
-    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-        "A"
-    )}</small></div>
-  </div>
-  <a href="/">All cities</a>
-  `;
+
+    function updateSelectedCityTime() {
+        let cityTime = moment().tz(cityTimeZone);
+        let citiesElement = document.querySelector("#cities");
+        citiesElement.innerHTML = `
+          <div class="city">
+            <div>
+              <h2>${cityName}</h2>
+              <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+            </div>
+            <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+            "A"
+        )}</small></div>
+          </div>
+          <a href="/">All cities</a>
+        `;
+    }
+
+    updateSelectedCityTime();  // Update immediately
+    selectedCityInterval = setInterval(updateSelectedCityTime, 1000);  // Update every second
 }
 
 updateTime();
